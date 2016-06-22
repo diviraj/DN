9958acd90dce0fdfdb9d61737d7a9bdf4a283611
DIVYA S <divyasundarraj1996@gmail.com>
2016-06-22 21:50:47 +0530

    Create firstrepeat.c

---
+++ firstrepeat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a[1000],b[100]={0},n,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+		for(i=0;i<n;i++)
+    	b[a[i]]++;
+    for(i=0;i<n;i++)
+    {
+    	if(b[a[i]]>1)
+    	{
+    		printf("%d",a[i]);
+    		break;
+    	}
+    }
+	return 0;
+}
