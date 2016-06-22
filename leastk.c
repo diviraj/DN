3ded07ae461dfedc51b9babb731252027ffafd61
DIVYA S <divyasundarraj1996@gmail.com>
2016-06-22 23:14:38 +0530

    Create leastk.c

---
+++ leastk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a[1000],n,i,j,t,k;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		scanf("%d",&a[i]);
+	scanf("%d",&k);
+	for(i=1;i<=n;i++)
+	{
+		for(j=i+1;j<=n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(i=1;i<=k;i++)
+		printf("%d",a[i]);
+	return 0;
+}
